d637d4fcfaaf60edb86df540797e73f7fe985e3b
NIE Zhenke <niezhenke@bestseller.com.cn>
2024-07-18 15:02:00 +0800

    init

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -15,7 +15,8 @@ const count = ref(0)
     <button type="button" @click="count++">count is {{ count }}</button>
     <p>
       Edit
-      <code>components/HelloWorld.vue</code>to test HMR
+      <code>components/HelloWorld.vue</code>
+      to test HMR
     </p>
   </div>
 
@@ -41,7 +42,6 @@ const count = ref(0)
 
 .box {
   border-radius: 5px;
-  padding: 10px;
   width: auto;
   height: auto;
   font-size: large;
